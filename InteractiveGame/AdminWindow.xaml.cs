6551c72cd814ed166abd9df70d3e6fcd868d1cd7
mopak <dobromirstefanov96@gmail.com>
2019-05-05 18:41:59 +0300

    Add delete user functionallity in admin panel

--- InteractiveGame/AdminWindow.xaml.cs
+++ InteractiveGame/AdminWindow.xaml.cs
@@ -1,27 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace InteractiveGame
 {
-    /// <summary>
-    /// Interaction logic for AdminWindow.xaml
-    /// </summary>
     public partial class AdminWindow : Window
     {
         public AdminWindow()
         {
             InitializeComponent();
+            PopulateUserBox();
+        }
+
+        public void PopulateUserBox()
+        {
+            UserBox.Items.Clear();
+
+            List<string> usernames = GetAllUsernames();
+
+            UserBox.SelectedIndex = 0;
+            foreach (string username in usernames)
+            {
+                UserBox.Items.Add(new ComboBoxItem { Content = username });
+            }
+        }
+
+        public void DeleteUserClick(object sender, RoutedEventArgs e)
+        {
+            DeleteByUsername(((ComboBoxItem)UserBox.SelectedItem).Content.ToString());
+            PopulateUserBox();
+        }
+
+        public List<string> GetAllUsernames()
+        {
+            return App.DbManager.GameUser.Where(u => u.IsAdmin == null || u.IsAdmin == false).Select(u => u.Username).ToList();
+        }
+
+        public void DeleteByUsername(string username)
+        {
+            GameUser user = App.DbManager.GameUser.FirstOrDefault(u => u.Username == username);
+            App.DbManager.GameUser.Remove(user);
+            App.DbManager.SaveChanges();
         }
     }
 }
