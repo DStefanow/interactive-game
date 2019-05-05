d19758955b14084226a0e94d2794dc8bd8599496
mopak <dobromirstefanov96@gmail.com>
2019-05-05 15:06:16 +0300

    Add global object for dqatabase manipulation

--- InteractiveGame/App.xaml.cs
+++ InteractiveGame/App.xaml.cs
@@ -8,10 +8,8 @@ using System.Windows;
 
 namespace InteractiveGame
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
+        public static Items DbManager = new Items();
     }
 }
