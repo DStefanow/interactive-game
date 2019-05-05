c19f4ae9d6cd6b6a49b0b97e6d67d64cd4ab80e1
mopak <dobromirstefanov96@gmail.com>
2019-05-05 14:40:52 +0300

    Add basic register functionallity

--- InteractiveGame/RegisterWindow.xaml.cs
+++ InteractiveGame/RegisterWindow.xaml.cs
@@ -1,21 +1,17 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data.Entity.Infrastructure;
+using System.Data.SqlClient;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace InteractiveGame
 {
     public partial class RegisterWindow : Window
     {
+        private const short USERNAME_LENGTH = 5;
+        private const short PASSWORD_LENGTH = 8;
+        private const short FULLNAME_LENGTH = 4;
+
         public RegisterWindow()
         {
             InitializeComponent();
@@ -30,7 +26,57 @@ namespace InteractiveGame
 
         private void RegisterClickEvent(object sender, RoutedEventArgs e)
         {
+            string username = UsernameBox.Text;
+            string password = PasswordBox.Password;
+            string fullName = FullNameBox.Text;
 
+            if (!IsValidInputParams(username, password, fullName))
+            {
+                return;
+            }
+
+            GameUser newUser = new GameUser(username, password, fullName);
+            App.DbManager.GameUser.Add(newUser);
+            
+            try
+            {
+                App.DbManager.SaveChanges();
+            }
+            catch (DbUpdateException ex)
+            {
+                if (ex.InnerException?.InnerException is SqlException sqlEx &&
+                    (sqlEx.Number == 2601 || sqlEx.Number == 2627))
+                {
+                    MessageBox.Show("Потребителско име: " + username + " вече същестува!");
+                    return;
+                }
+            }
+
+            LoginWindow logWindow = new LoginWindow();
+            this.Close();
+            logWindow.Show();
+        }
+
+
+        private bool IsValidInputParams(string username, string password, string fullName)
+        {
+            if (username.Length < USERNAME_LENGTH)
+            {
+                MessageBox.Show("Потребителското име трябва да е поне " + USERNAME_LENGTH + " символа");
+                return false;
+            }
+            else if (password.Length < PASSWORD_LENGTH)
+            {
+                MessageBox.Show("Паролата трябва да е поне " + PASSWORD_LENGTH + " символа");
+                return false;
+            }
+            else if (fullName.Length < FULLNAME_LENGTH)
+            {
+                MessageBox.Show("Името трябва да е поне " + FULLNAME_LENGTH + " символа");
+                return false;
+            }
+
+            return true;
         }
     }
 }
