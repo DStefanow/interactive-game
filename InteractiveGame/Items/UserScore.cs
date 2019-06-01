ba148768c70050d6cb0db1a280f70414c2271254
mopak <dobromirstefanov96@gmail.com>
2019-06-01 17:39:00 +0300

    Fix problem with EF UserScore obj

--- InteractiveGame/Items/UserScore.cs
+++ InteractiveGame/Items/UserScore.cs
@@ -6,18 +6,20 @@ namespace InteractiveGame
     using System.ComponentModel.DataAnnotations.Schema;
     using System.Data.Entity.Spatial;
 
+    [Table("user_score")]
     public partial class UserScore
     {
         [Key]
         [Column("user_id", Order = 0)]
         [DatabaseGenerated(DatabaseGeneratedOption.None)]
-        public int UserId { get; set; }
+        public int? UserId { get; set; }
 
         [Key]
         [Column("category_id", Order = 1)]
         [DatabaseGenerated(DatabaseGeneratedOption.None)]
-        public int CategoryId { get; set; }
+        public int? CategoryId { get; set; }
 
+        [Column("score")]
         public int? Score { get; set; }
 
         public virtual Category Category { get; set; }
