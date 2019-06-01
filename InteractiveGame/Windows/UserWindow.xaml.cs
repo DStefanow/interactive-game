8a6daca16ce1e0ed931dec252e88bc483240bb53
mopak <dobromirstefanov96@gmail.com>
2019-06-01 17:39:23 +0300

    Add more logic for use window

--- InteractiveGame/Windows/UserWindow.xaml.cs
+++ InteractiveGame/Windows/UserWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -26,6 +27,7 @@ namespace InteractiveGame
 
             PopulateUserInfo();
             PopulateCategoryBox();
+            PopulateUserScoreBox(user.Id);
         }
 
         private void ExitButtonClick(object sender, RoutedEventArgs e)
@@ -37,12 +39,23 @@ namespace InteractiveGame
 
         private void StartTopicClick(object sender, RoutedEventArgs e)
         {
-
+            // TODO ...
         }
 
         private void CategoryChange(object sender, SelectionChangedEventArgs e)
         {
-            Console.WriteLine();
+            TopicBox.Items.Clear();
+
+            string selectedName = ((ComboBoxItem)CategoryBox.SelectedItem).Name.ToString();
+            int categoryId = Int32.Parse(Regex.Match(selectedName, @"\d+").Value);
+            
+            List<Topic> topicsForCategory = GetAllTopicsForGivenCategory(categoryId);
+
+            TopicBox.SelectedIndex = 0;
+            foreach (Topic topic in topicsForCategory)
+            {
+                TopicBox.Items.Add(new ComboBoxItem { Content = topic.Title, Name = "Id" + topic.Id });
+            }
         }
 
         private void PopulateUserInfo()
@@ -67,6 +80,45 @@ namespace InteractiveGame
         private List<Category> GetAllCategories()
         {
             return App.DbManager.Category.Select(x => x).ToList();
+        }
+
+        private List<Topic> GetAllTopicsForGivenCategory(int currentCategoryId)
+        {
+            return App.DbManager.Topic.Where(x => x.CategoryId == currentCategoryId).ToList();
+        }
+
+        private void PopulateUserScoreBox(int userId)
+        {
+            List<Category> allCategories = App.DbManager.Category.Select(x => x).ToList();
+
+            int currentScore = 0;
+            string resultData;
+            foreach (Category cat in allCategories)
+            {
+                UserScore currentResultByCategory = App.DbManager.UserScore.
+                    FirstOrDefault(x => x.UserId == userId && x.CategoryId == cat.Id);
+
+                if (currentResultByCategory == null ||
+                     currentResultByCategory.Score == 0 ||
+                     currentResultByCategory.Score == null)
+                {
+                    currentScore = 0;
+                }
+                else
+                {
+                    currentScore = (int)currentResultByCategory.Score;
+                }
+
+                resultData = cat.CategoryName +  " - " + currentScore;
+
+                ListBoxItem item = new ListBoxItem
+                {
+                    Name = "Id" + cat.Id,
+                    Content = resultData
+                };
+
+                PointOnCategory.Items.Add(new ListBox);
+            }
         }
     }
 }
