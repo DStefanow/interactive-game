9a265560549d2baf4f7052201d42b237f6e595d4
mopak <dobromirstefanov96@gmail.com>
2019-06-09 13:11:30 +0300

    Update create qa logic

--- InteractiveGame/Windows/CreateQAWindow.xaml.cs
+++ InteractiveGame/Windows/CreateQAWindow.xaml.cs
@@ -1,27 +1,68 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace InteractiveGame
 {
-    /// <summary>
-    /// Interaction logic for CreateQAWindow.xaml
-    /// </summary>
     public partial class CreateQAWindow : Window
     {
         public CreateQAWindow()
         {
             InitializeComponent();
+            PopulateCategoryBox();
+        }
+
+        public void BackButtonClick(object sender, RoutedEventArgs e)
+        {
+            AdminWindow adminWindow = new AdminWindow();
+            this.Close();
+            adminWindow.Show();
+            return;
+        }
+
+        private void PopulateCategoryBox()
+        {
+            CategoryBox.Items.Clear();
+
+            List<Category> categories = GetAllCategoriesWithTopics();
+
+            CategoryBox.SelectedIndex = 0;
+            foreach (Category category in categories)
+            {
+                CategoryBox.Items.Add(new ComboBoxItem { Content = category.CategoryName, Name = "Id" + category.Id });
+            }
+        }
+        private void CategoryChange(object sender, SelectionChangedEventArgs e)
+        {
+            TopicBox.Items.Clear();
+
+            string selectedName = ((ComboBoxItem)CategoryBox.SelectedItem).Name.ToString();
+            int categoryId = Int32.Parse(Regex.Match(selectedName, @"\d+").Value);
+
+            List<Topic> topicsForCategory = GetAllTopicsForGivenCategory(categoryId);
+
+            TopicBox.SelectedIndex = 0;
+            foreach (Topic topic in topicsForCategory)
+            {
+                TopicBox.Items.Add(new ComboBoxItem { Content = topic.Title, Name = "Id" + topic.Id });
+            }
+        }
+
+        private List<Category> GetAllCategoriesWithTopics()
+        {
+            return App.DbManager.Category.SqlQuery("SELECT c.id, c.category_name as CategoryName FROM category c " + 
+                    "JOIN topic t " +
+                    "ON c.id = t.category_id " +
+                    "GROUP BY c.id, c.category_name").
+                    ToList();
+        }
+
+        private List<Topic> GetAllTopicsForGivenCategory(int currentCategoryId)
+        {
+            return App.DbManager.Topic.Where(x => x.CategoryId == currentCategoryId).ToList();
         }
     }
 }
