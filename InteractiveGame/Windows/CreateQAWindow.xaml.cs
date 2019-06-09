ca4fed0b38a991168ba4440a8d48be9766944973
mopak <dobromirstefanov96@gmail.com>
2019-06-09 14:10:28 +0300

    Add logic for first question answer

--- InteractiveGame/Windows/CreateQAWindow.xaml.cs
+++ InteractiveGame/Windows/CreateQAWindow.xaml.cs
@@ -23,6 +23,23 @@ namespace InteractiveGame
             return;
         }
 
+        public void InsertButtonClick(object sender, RoutedEventArgs e)
+        {
+            string selectedName = ((ComboBoxItem)TopicBox.SelectedItem).Name.ToString();
+            int topicId = Int32.Parse(Regex.Match(selectedName, @"\d+").Value);
+
+            // Delete all questions with answer for the given topic (update option)
+            App.DbManager.Question.RemoveRange(
+                App.DbManager.Question.Where(
+                    x => x.TopicId == topicId
+                )
+            );
+            App.DbManager.SaveChanges();
+
+
+            InsertFirstQuestionAnswers(topicId);
+        }
+
         private void PopulateCategoryBox()
         {
             CategoryBox.Items.Clear();
@@ -58,6 +75,31 @@ namespace InteractiveGame
                     "ON c.id = t.category_id " +
                     "GROUP BY c.id, c.category_name").
                     ToList();
+        }
+
+        private void InsertFirstQuestionAnswers(int topicId)
+        {
+            // First Question
+            string firstQuestion = this.FirstQuestion.Text;
+            int firstPoints = Int32.Parse(this.FirstPoints.Text);
+
+            // First Answers
+            string firstCorrect = this.FirstCorrect.Text;
+            string firstWrongOne = this.FirstWrongOne.Text;
+            string firstWrongTwo = this.FirstWrongTwo.Text;
+            string firstWrongThree = this.FirstWrongThree.Text;
+
+            Question question = new Question(topicId, firstQuestion, firstPoints);
+            App.DbManager.Question.Add(question);
+            App.DbManager.SaveChanges();
+
+            // Add answers
+            App.DbManager.Answer.Add(new Answer(question.Id, firstCorrect, true));
+            App.DbManager.Answer.Add(new Answer(question.Id, firstWrongOne, false));
+            App.DbManager.Answer.Add(new Answer(question.Id, firstWrongTwo, false));
+            App.DbManager.Answer.Add(new Answer(question.Id, firstWrongThree, false));
+
+            App.DbManager.SaveChanges();
         }
 
         private List<Topic> GetAllTopicsForGivenCategory(int currentCategoryId)
