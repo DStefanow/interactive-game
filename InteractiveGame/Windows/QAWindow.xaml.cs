f21abe0343edb36ac70fccf75b576a3708b63422
mopak <dobromirstefanov96@gmail.com>
2019-06-11 23:54:31 +0300

    Add QA button logic and populate questions for given topic

--- InteractiveGame/Windows/QAWindow.xaml.cs
+++ InteractiveGame/Windows/QAWindow.xaml.cs
@@ -16,9 +16,46 @@ namespace InteractiveGame
 {
     public partial class QAWindow : Window
     {
+        GameUser currentUser;
+        Topic currentTopic;
+
         public QAWindow(GameUser currentUser, Topic currentTopic)
         {
+            this.currentUser = currentUser;
+            this.currentTopic = currentTopic;
+            
             InitializeComponent();
+            PopulateQuestionsAndAnswers();
+        }
+
+        private void BackButtonClick(object sender, RoutedEventArgs e)
+        {
+            UserWindow userWindow = new UserWindow(currentUser);
+            this.Close();
+            userWindow.Show();
+            return;
+        }
+
+        private void SubmitClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void PopulateQuestionsAndAnswers()
+        {
+            TopicTitleLabel.Content = currentTopic.Title.ToString();
+
+            Question[] questions = GetQuestionsForTopic(currentTopic);
+
+            QuestionOneLabel.Content = questions[0].Description.ToString();
+            QuestionTwoLabel.Content = questions[1].Description.ToString();
+            QuestionThreeLabel.Content = questions[2].Description.ToString();
+            QuestionFourthLabel.Content = questions[3].Description.ToString();
+        }
+
+        private Question[] GetQuestionsForTopic(Topic topic)
+        {
+            return App.DbManager.Question.Where(x => x.TopicId == topic.Id).OrderBy(x => x.Id).ToArray();
         }
     }
 }
